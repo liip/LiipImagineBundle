927b5f2ec11a24b239c2888c632ea40dc033eb4e
Jeremy Mikola <jmikola@gmail.com>
2011-02-11 16:10:07 -0500

    [DependencyInjection] Update extension class to use FileLocator

--- DependencyInjection/ImagineExtension.php
+++ DependencyInjection/ImagineExtension.php
@@ -5,13 +5,14 @@ namespace Bundle\Avalanche\ImagineBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\FileLocator;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class ImagineExtension extends Extension
 {
     public function configLoad(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, __DIR__.'/../Resources/config');
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('imagine.xml');
 
         $config = $this->mergeConfig($configs);
