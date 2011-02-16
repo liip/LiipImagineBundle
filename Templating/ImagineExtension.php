663676c5902474991d95617d6a62dba3c93dae68
Bulat Shakirzyanov <mallluhuct@gmail.com>
2011-02-16 16:36:17 -0500

    fixed error handling to throw more helpful exceptions

--- Templating/ImagineExtension.php
+++ Templating/ImagineExtension.php
@@ -5,6 +5,7 @@ namespace Avalanche\Bundle\ImagineBundle\Templating;
 use Avalanche\Bundle\ImagineBundle\Imagine\FilterManager;
 use Imagine\ImagineInterface;
 use Imagine\Filter\FilterInterface;
+use Imagine\Exception\Exception;
 use Symfony\Bundle\FrameworkBundle\Util\Filesystem;
 
 class ImagineExtension extends \Twig_Extension
@@ -14,14 +15,16 @@ class ImagineExtension extends \Twig_Extension
     private $filesystem;
     private $webRoot;
     private $cacheDir;
+    private $defaultExtension;
 
-    public function __construct(ImagineInterface $imagine, FilterManager $filterManager, Filesystem $filesystem, $webRoot, $cacheDir)
+    public function __construct(ImagineInterface $imagine, FilterManager $filterManager, Filesystem $filesystem, $webRoot, $cacheDir, $defaultExtension = 'png')
     {
-        $this->imagine       = $imagine;
-        $this->filterManager = $filterManager;
-        $this->filesystem    = $filesystem;
-        $this->webRoot       = $webRoot;
-        $this->cacheDir      = $cacheDir;
+        $this->imagine          = $imagine;
+        $this->filterManager    = $filterManager;
+        $this->filesystem       = $filesystem;
+        $this->webRoot          = $webRoot;
+        $this->cacheDir         = $cacheDir;
+        $this->defaultExtension = $defaultExtension;
 
         if (0 !== strpos($this->cacheDir, $this->webRoot)) {
             throw new \InvalidArgumentException('Looks like the specified "cache_dir" is not in the "web_root", please make sure imagine "cache_dir" is publicly accessible');
@@ -39,11 +42,12 @@ class ImagineExtension extends \Twig_Extension
     {
         $cacheFile = md5($path);
         $ext       = pathinfo($path, PATHINFO_EXTENSION);
+        $ext       = $ext ? $ext : $this->defaultExtension;
         $cachePath = $this->cacheDir.'/'.$filter.'/'.$cacheFile[0].'/'.$cacheFile[1].'/'.$cacheFile.'.'.$ext;
 
         if (!file_exists($cachePath)) {
             $filter = $this->filterManager->get($filter);
-            if (0 === strpos($path, '/')) {
+            if (0 === strpos($path, '/') && !file_exists($path)) {
                 $path = $this->webRoot . $path;
             }
 
@@ -52,9 +56,20 @@ class ImagineExtension extends \Twig_Extension
             $dir = dirname($cachePath);
             if (!is_dir($dir)) {
                 $this->filesystem->mkdirs($dir);
+                if (!is_dir($dir)) {
+                    throw new \RuntimeException(sprintf('cannot write to '.
+                        '"%s", please check permissions', $this->cacheDir));
+                }
             }
 
-            $filter->apply($image)->save($cachePath);
+            try {
+                $filter->apply($image)->save($cachePath);
+            } catch (Exception $e) {
+                throw new \RuntimeException(sprintf('Could not save '.
+                    'processed image at "%s", make sure the source image url '.
+                    'is correctly formatted and has file extension %s', $cachePath, $ext
+                ));
+            }
 
             unset($filter, $image);
         }
