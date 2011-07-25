8a73e0aab30cc5c07f22a50d596c87a3273e140e
Bulat Shakirzyanov <mallluhuct@gmail.com>
2011-07-25 14:13:09 -0700

    fix typehint

--- Imagine/Filter/Loader/ThumbnailFilterLoader.php
+++ Imagine/Filter/Loader/ThumbnailFilterLoader.php
@@ -3,6 +3,7 @@
 namespace Avalanche\Bundle\ImagineBundle\Imagine\Filter\Loader;
 
 use Imagine\Image\Box;
+use Imagine\Image\ManipulatorInterface;
 use Imagine\Filter\Basic\Thumbnail;
 
 class ThumbnailFilterLoader implements LoaderInterface
@@ -10,8 +11,8 @@ class ThumbnailFilterLoader implements LoaderInterface
     public function load(array $options = array())
     {
         $mode = $options['mode'] === 'inset' ?
-            ImageInterface::THUMBNAIL_INSET :
-            ImageInterface::THUMBNAIL_OUTBOUND;
+            ManipulatorInterface::THUMBNAIL_INSET :
+            ManipulatorInterface::THUMBNAIL_OUTBOUND;
 
         list($width, $height) = $options['size'];
 
