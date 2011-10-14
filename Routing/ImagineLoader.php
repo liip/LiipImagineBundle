48c714f9accce282d2cadc4a84b90b9d427e8d3c
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-10-14 15:50:58 +0200

    better handle empty path

--- Routing/ImagineLoader.php
+++ Routing/ImagineLoader.php
@@ -33,7 +33,9 @@ class ImagineLoader extends Loader
             foreach ($this->filters as $filter => $config) {
                 $pattern = $this->cachePrefix;
                 if (isset($config['path'])) {
-                    $pattern .= '/'.trim($config['path'], '/');
+                    if ('/' !== $config['path']) {
+                        $pattern .= '/'.trim($config['path'], '/');
+                    }
                 } elseif ('' !== $filter) {
                     $pattern .= '/'.$filter;
                 }
