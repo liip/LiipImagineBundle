101b6b847fe7803913c036ec81bacbca36447417
Jeremy Mikola <jmikola@gmail.com>
2011-11-11 12:27:23 -0500

    Add Resize and RelativeResize filters
    Ported these over from JmikolaImagineBundle, with some modifications to accomodate LiipImagineBundle's LoaderInterface. These can potentially replace the existing Thumbnail filter.

---
+++ Imagine/Filter/Loader/ResizeFilterLoader.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Liip\ImagineBundle\Imagine\Filter\Loader;
+
+use Imagine\Filter\Basic\Resize;
+use Imagine\Image\Box;
+
+/**
+ * Loader for Imagine's basic resize filter.
+ *
+ * @author Jeremy Mikola <jmikola@gmail.com>
+ */
+class ResizeFilterLoader implements LoaderInterface
+{
+    /**
+     * @see Liip\ImagineBundle\Imagine\Filter\Loader\LoaderInterface::load()
+     */
+    public function load(ImageInterface $image, array $options = array())
+    {
+        list($width, $height) = $options['size'];
+
+        $filter = new Resize(new Box($width, $height));
+
+        return $filter->apply($image);
+    }
+}
