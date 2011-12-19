b31e984b9f8fb95bc4a7af71787a3a2a2e60630d
lsmith77 <smith@pooteeweet.org>
2011-12-19 11:49:58 +0100

    add twig to the vendors

--- vendor/vendors.php
+++ vendor/vendors.php
@@ -11,6 +11,7 @@ $vendorDir = __DIR__;
 $deps = array(
     array('symfony', 'http://github.com/symfony/symfony', isset($_SERVER['SYMFONY_VERSION']) ? $_SERVER['SYMFONY_VERSION'] : 'origin/master'),
     array('imagine', 'http://github.com/avalanche123/Imagine', 'v0.2.6'),
+    array('twig', 'http://github.com/fabpot/twig', 'origin/master'),
 );
 
 foreach ($deps as $dep) {
