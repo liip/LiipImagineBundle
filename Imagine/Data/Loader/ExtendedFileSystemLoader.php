80c6fac6e4c706ab309423e08804e9c79dac6d78
Luca Saba <lucasaba@gmail.com>
2012-03-13 19:21:00 +0100

    Adds transformers concept to the data loader

---
+++ Imagine/Data/Loader/ExtendedFileSystemLoader.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Liip\ImagineBundle\Imagine\Data\Loader;
+
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+use Imagine\Image\ImagineInterface;
+
+class ExtendedFileSystemLoader extends FileSystemLoader
+{
+    /**
+     *
+     * @var array 
+     */
+    private $transformers;
+
+    /**
+     * Constructs
+     *
+     * @param ImagineInterface  $imagine
+     * @param array             $formats
+     * @param string            $rootPath
+     * @param array             $transformers
+     */
+    public function __construct(ImagineInterface $imagine, $formats, $rootPath, array $transformers)
+    {
+        parent::__construct($imagine, $formats, $rootPath);
+        $this->transformers = $transformers;
+    }
+
+    /**
+     * Get the file info for the given path
+     *
+     * This can optionally be used to generate the given file
+     *
+     * @param $absolutePath
+     * @return array
+     */
+    protected function getFileInfo($absolutePath)
+    {
+        if (!empty($this->transformers)) {
+            foreach ($this->transformers as $transformer) {
+                $absolutePath = $transformer->applyTransform($absolutePath);
+            }
+        }
+        return pathinfo($absolutePath);
+    }
+}
--- Imagine/Data/Loader/FileSystemLoader.php
+++ Imagine/Data/Loader/FileSystemLoader.php
@@ -22,12 +22,6 @@ class FileSystemLoader implements LoaderInterface
      * @var string
      */
     protected $rootPath;
-    
-    /**
-     *
-     * @var array 
-     */
-    private $transformers;
 
     /**
      * Constructs
@@ -35,14 +29,12 @@ class FileSystemLoader implements LoaderInterface
      * @param ImagineInterface  $imagine
      * @param array             $formats
      * @param string            $rootPath
-     * @param array             $transformers
      */
-    public function __construct(ImagineInterface $imagine, $formats, $rootPath, $transformers)
+    public function __construct(ImagineInterface $imagine, $formats, $rootPath)
     {
         $this->imagine = $imagine;
         $this->formats = $formats;
         $this->rootPath = realpath($rootPath);
-        $this->transformers = $transformers;
     }
 
     /**
@@ -71,13 +63,6 @@ class FileSystemLoader implements LoaderInterface
 
         $info = $this->getFileInfo($this->rootPath.'/'.ltrim($path, '/'));
         $absolutePath = $info['dirname'].'/'.$info['basename'];
-
-        if (!empty($this->transformers)) {
-            foreach ($this->transformers as $transformer) {
-                $absolutePath = $transformer->applyTransform($absolutePath);
-            }
-            $info = $this->getFileInfo($absolutePath);
-        }
 
         $name = $info['dirname'].'/'.$info['filename'];
         $targetFormat = empty($this->formats) || in_array($info['extension'], $this->formats)
