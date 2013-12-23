76864e46a36b930a52f5fa18f349425e6284b7c5
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2013-12-23 16:50:53 +0200

    [data loader] rework to return binary instance. add mime type guesser.

---
+++ Tests/Functional/MimeTypeGuesserTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Liip\ImagineBundle\Tests\Functional;
+
+class MimeTypeGuesserTest extends WebTestCase
+{
+    public function testCouldBeGetFromContainerAsService()
+    {
+        $this->createClient();
+        $guesser = self::$kernel->getContainer()->get('liip_imagine.mime_type_guesser');
+
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\File\MimeType\MimeTypeGuesser', $guesser);
+    }
+} 
