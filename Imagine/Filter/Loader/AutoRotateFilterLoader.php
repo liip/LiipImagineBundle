da37f988d252d6a7c7ab580de1ad159bb91f91b3
Robert Schönthal <caziel@gmx.net>
2014-01-29 10:03:33 +0100

    some cosmetics

--- Imagine/Filter/Loader/AutoRotateFilterLoader.php
+++ Imagine/Filter/Loader/AutoRotateFilterLoader.php
@@ -4,30 +4,23 @@ namespace Liip\ImagineBundle\Imagine\Filter\Loader;
 
 use Imagine\Image\ImageInterface;
 
+/**
+ * AutoRotateFilterLoader - rotates an Image based on its EXIF Data
+ *
+ * @author Robert Schönthal <robert.schoenthal@gmail.com>
+ */
 class AutoRotateFilterLoader implements LoaderInterface
 {
     /**
      * {@inheritDoc}
      */
-    function load(ImageInterface $image, array $options = array())
+    public function load(ImageInterface $image, array $options = array())
     {
         $exifData = exif_read_data("data://image/jpeg;base64," . base64_encode($image->get('jpg')));
 
         if (isset($exifData['Orientation'])) {
-            $orientation = (int)$exifData['Orientation'];
-
-            $degree = 0;
-            switch ($orientation) {
-                case 8:
-                    $degree = -90;
-                    break;
-                case 3:
-                    $degree = 180;
-                    break;
-                case 6:
-                    $degree = 90;
-                    break;
-            }
+            $orientation = (int) $exifData['Orientation'];
+            $degree = $this->calculateRotation($orientation);
 
             if ($degree !== 0) {
                 $image->rotate($degree);
@@ -35,5 +28,31 @@ class AutoRotateFilterLoader implements LoaderInterface
         }
 
         return $image;
+    }
+
+    /**
+     * calculates to rotation degree from the EXIF Orientation
+     *
+     * @param  int $orientation
+     * @return int
+     */
+    private function calculateRotation($orientation)
+    {
+        switch ($orientation) {
+            case 8:
+                $degree = -90;
+                break;
+            case 3:
+                $degree = 180;
+                break;
+            case 6:
+                $degree = 90;
+                break;
+            default:
+                $degree = 0;
+                break;
+        }
+
+        return $degree;
     }
 }
