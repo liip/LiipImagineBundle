af1868f613cffccb851a27e6b223b4d02bdff0f6
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2014-02-03 12:03:07 +0200

    [twig] do not escape filter url.

--- Imagine/Cache/CacheManager.php
+++ Imagine/Cache/CacheManager.php
@@ -137,11 +137,7 @@ class CacheManager
 
         $params = array('path' => ltrim($path, '/'));
 
-        return str_replace(
-            urlencode($params['path']),
-            urldecode($params['path']),
-            $this->router->generate('_imagine_'.$filter, $params, $absolute)
-        );
+        return $this->router->generate('_imagine_'.$filter, $params, $absolute);
     }
 
     /**
