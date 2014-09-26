35851b9a4a6121546f53efdb86200ccb830ea25f
michael.schramm <michael.schramm@gmail.com>
2014-09-26 17:06:27 +0200

    * implement interlace filter * add functional and unit test * move functional tests (DataManager and FilterManager) to subfolders

---
+++ Tests/Functional/Imagine/Filter/Loader/InterlaceFilterLoaderTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace Liip\ImagineBundle\Tests\Functional\Imagine\Filter\Loader;
+
+use Liip\ImagineBundle\Tests\Functional\WebTestCase;
+
+class InterlaceFilterLoaderTest extends WebTestCase
+{
+    public function testCouldBeGetFromContainerAsService()
+    {
+        $this->createClient();
+        $service = self::$kernel->getContainer()->get('liip_imagine.filter.loader.interlace');
+
+        $this->assertInstanceOf('Liip\ImagineBundle\Imagine\Filter\Loader\InterlaceFilterLoader', $service);
+    }
+}
+
