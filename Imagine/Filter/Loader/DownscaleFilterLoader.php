0afaf978182a9118f6d90dc30dc428f15951ae21
Sascha <sascha.meissner@docinsider.de>
2015-06-19 13:34:52 +0200

    introduced DownscaleFilterLoader

---
+++ Imagine/Filter/Loader/DownscaleFilterLoader.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Liip\ImagineBundle\Imagine\Filter\Loader;
+
+use Imagine\Filter\Basic\Resize;
+use Imagine\Image\ImageInterface;
+use Imagine\Image\Box;
+
+
+/**
+ * downscale filter
+ *
+ */
+class DownscaleFilterLoader implements LoaderInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ImageInterface $image, array $options = array())
+    {
+        if (!isset($options['max'])) {
+            throw new \InvalidArgumentException('Missing max option.');
+        }
+
+        list($width, $height) = $options['max'];
+
+        $size = $image->getSize();
+        $origWidth = $size->getWidth();
+        $origHeight = $size->getHeight();
+
+        if ($origWidth > $width || $origHeight > $height) {
+
+            $widthRatio = $width / $origWidth ;
+            $heightRatio = $height / $origHeight;
+
+            $ratio = $widthRatio > $heightRatio ? $widthRatio : $heightRatio;
+
+            $filter = new Resize(new Box($origWidth * $ratio, $origHeight * $ratio));
+
+            return $filter->apply($image);
+        }
+
+        return $image;
+    }
+}
