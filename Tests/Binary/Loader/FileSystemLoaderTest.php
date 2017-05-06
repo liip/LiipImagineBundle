81f98561579097375211c3be2c46d256c0ff7b56
Rob Frawley 2nd <rmf@src.run>
2017-05-06 17:06:08 -0400

    deprecate routing xml file in favor of yaml

--- Tests/Binary/Loader/FileSystemLoaderTest.php
+++ Tests/Binary/Loader/FileSystemLoaderTest.php
@@ -173,7 +173,7 @@ class FileSystemLoaderTest extends \PHPUnit_Framework_TestCase
     public function provideOutsideRootPathsData()
     {
         return array(
-            array('../Loader/../../Binary/Loader/../../../Resources/config/routing.xml'),
+            array('../Loader/../../Binary/Loader/../../../Resources/config/routing.yaml'),
             array('../../Binary/'),
         );
     }
