f7608bdedf6958e584e5a4ee37271b1564d81bbc
Andrey Suslin <andrey.suslin@intexsys.lv>
2022-03-21 11:00:59 +0200

    Update requirements Symfony 4+

--- DependencyInjection/Factory/Resolver/FormatResolverFactory.php
+++ DependencyInjection/Factory/Resolver/FormatResolverFactory.php
@@ -19,14 +19,14 @@ class FormatResolverFactory extends WebPathResolverFactory
      */
     public function create(ContainerBuilder $container, $resolverName, array $config)
     {
-        $resolverDefinition = new DefinitionDecorator('liip_imagine.cache.resolver.prototype.format');
+        $resolverDefinition = $this->getChildResolverDefinition('format');
         $resolverDefinition->replaceArgument(2, $config['web_root']);
         $resolverDefinition->replaceArgument(3, $config['cache_prefix']);
-        $resolverDefinition->addTag('liip_imagine.cache.resolver', array(
+        $resolverDefinition->addTag('liip_imagine.cache.resolver', [
             'resolver' => $resolverName
-        ));
-        $resolverId = 'liip_imagine.cache.resolver.'.$resolverName;
+        ]);
 
+        $resolverId = 'liip_imagine.cache.resolver.'.$resolverName;
         $container->setDefinition($resolverId, $resolverDefinition);
 
         return $resolverId;
