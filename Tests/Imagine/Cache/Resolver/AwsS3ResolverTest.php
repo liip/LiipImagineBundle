118d41993c2d47d8caf21c450c980cf00b0f88bb
David Buchmann <david.buchmann@liip.ch>
2024-02-26 15:59:25 +0100

    fix cs issue

--- Tests/Imagine/Cache/Resolver/AwsS3ResolverTest.php
+++ Tests/Imagine/Cache/Resolver/AwsS3ResolverTest.php
@@ -344,7 +344,7 @@ class AwsS3ResolverTest extends AbstractTest
     }
 
     /**
-     * @return MockObject|\Aws\S3\S3Client
+     * @return MockObject|S3Client
      */
     protected function getS3ClientMock()
     {
